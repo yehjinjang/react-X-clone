0069578d7b90d8fc4b1e7108851ecbab6a08f8cd
yehjinjang <shj06261@kookmin.ac.kr>
2025-02-27 14:26:28 +0900

    initial commit (routing)

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Layout />,
+    children: [
+      {
+        path:"",
+        element: <Home />,
+      },
+      {
+        path:"profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+
+  },
+  {
+    path:"/create-account",
+    element: <CreateAccount />,
+  }
+
+]);
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: #000000;
+    color: white;
+    font-family: system-ui, appl-system, BlinkMacSystemFont, 'Segoe UI', 
+    Roboto, Ubuntu, 'Helvetica Neue', 
+    sans-serif;
+  }
+`;
+function App() {
+  return <>
+  <GlobalStyles />
+  <RouterProvider router={router} />
+  </>;
+}
+
+export default App;
