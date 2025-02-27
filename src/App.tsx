9e2a462586ee1d9cb602909d7c44a2dcaf9ef25f
yehjinjang <shj06261@kookmin.ac.kr>
2025-02-27 16:42:16 +0900

    style: form and UI 2

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./routes/home";
 import Profile from "./routes/profile";
 import Login from "./routes/login";
 import CreateAccount from "./routes/create-account";
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import { useEffect, useState } from "react";
 import LoadingScreen from "./components/loading-screen";
@@ -49,6 +49,11 @@ const GlobalStyles = createGlobalStyle`
     sans-serif;
   }
 `;
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
 function App() {
   const[isLoading, setIsLoading] = useState(true);
   const init = async() => {
@@ -60,10 +65,10 @@ function App() {
     init();
   },[]);
   return (
-  <>
+  <Wrapper>
   <GlobalStyles />
   {isLoading ? <LoadingScreen/> : <RouterProvider router = {router} />}
-  </>
+  </Wrapper>
   );
 }
 
--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,15 +1,44 @@
 import { styled } from "styled-components";
 import React, { useState } from 'react';
+import { setLogLevel } from "firebase/app";
 
-const Wrapper = styled.div``;
-const Form = styled.form``;
-const Input = styled.input``;
+const Wrapper = styled.div`
+height: 100vh;
+display: flex;
+flex-direction: column;
+align-items: center;
+width: 420px;
+padding: 50px 0px;`;
+
+const Form = styled.form`
+margin-top: 50px;
+display: flex;
+flex-direction: column;
+gap: 20px;
+width: 100%;
+`;
+const Input = styled.input`
+padding: 10px 20px;
+border-radius: 50px;
+border: none;
+width: 100%;
+font-size: 16px;
+&[type="submit"]{
+cursor: pointer;
+&:hover{
+opacity: 0.8;
+}
+}
+`;
+const Title = styled.h1`
+font_size: 42px;`;
 
 export default function CreateAccount(){
     const [isLoading, setIsLoading] = useState(false);
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const {target: {name, value}} = e;
         if(name == "name"){
@@ -23,15 +52,30 @@ export default function CreateAccount(){
     }
     const onSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        try{
+        /// create account
+        // set the name of the user. 
+        // redirect to the home page
+
+        }catch(e){
+            //setError
+        }
+        finally{
+            setIsLoading(false);
+        }
         console.log(name, email, password);
     };
     return <Wrapper>
+        <Title>Log into 𝕏</Title>
         <Form onSubmit = {onSubmit}>
             <Input onChange = {onChange} name= "name" value = {name} placeholder = "Name" type="text"required/>
             <Input onChange = {onChange} name= "email" placeholder = "Email" type="email" required/>
             <Input onChange = {onChange} name="password" placeholder="Password" type = "password" required/>
     
-            <Input type = "submit" value = "Create Account"/>
+            <Input 
+            type = "submit"
+            value = {isLoading? "Loading..." : "Create Account"}
+            />
         </Form>
     </Wrapper>
 }
