de3b1714a8fa27753600deea2533fe821fb059ac
yehjinjang <shj06261@kookmin.ac.kr>
2025-02-27 16:09:26 +0900

    style: form and UI

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,3 +1,38 @@
+import { styled } from "styled-components";
+import React, { useState } from 'react';
+
+const Wrapper = styled.div``;
+const Form = styled.form``;
+const Input = styled.input``;
+
 export default function CreateAccount(){
-    return <h1>create account</h1>;
+    const [isLoading, setIsLoading] = useState(false);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {target: {name, value}} = e;
+        if(name == "name"){
+            setName(value);
+        } else if (name == "email"){
+            setEmail(value);
+        }
+        else if (name == "password"){ 
+            setPassword(value);
+        }
+    }
+    const onSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(name, email, password);
+    };
+    return <Wrapper>
+        <Form onSubmit = {onSubmit}>
+            <Input onChange = {onChange} name= "name" value = {name} placeholder = "Name" type="text"required/>
+            <Input onChange = {onChange} name= "email" placeholder = "Email" type="email" required/>
+            <Input onChange = {onChange} name="password" placeholder="Password" type = "password" required/>
+    
+            <Input type = "submit" value = "Create Account"/>
+        </Form>
+    </Wrapper>
 }
+
