07088a32e82c69135941e9dc3e17e5543fa3e820
yehjinjang <shj06261@kookmin.ac.kr>
2025-03-12 14:12:36 +0900

    chore: firebase extension

---
+++ src/routes/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDtjqarp5AU-3eStito3LLgGubYxwfvkTg",
+  authDomain: "x-reloaded-d26ac.firebaseapp.com",
+  projectId: "x-reloaded-d26ac",
+  storageBucket: "x-reloaded-d26ac.firebasestorage.app",
+  messagingSenderId: "219844408065",
+  appId: "1:219844408065:web:8ab66782bb18686eb62100"
+};
+// initialize firebase and getting the auth object
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
