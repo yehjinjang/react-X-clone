d2bcef984e712ea2114e64dc2a667a46e9e3345d
yehjinjang <shj06261@kookmin.ac.kr>
2025-03-12 17:31:46 +0900

    feat: post tweet form

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,11 +1,20 @@
 import { auth } from "../routes/firebase";
+import PostTweetForm from "../components/post-tweet-form";
+import { styled } from "styled-components"
+
+const Wrapper = styled.div``;
 
 export default function Home() {
-    const logOut = () => {
-        auth.signOut();
-    }
-    return (<h1>
-        <button onClick={logOut}>Log Out</button>
-    </h1>
-);
+    return(
+        <Wrapper>
+         <PostTweetForm/>
+        </Wrapper>
+    );
+    // const logOut = () => {
+    //     auth.signOut();
+    // }
+    // return (<h1>
+    //     <button onClick={logOut}>Log Out</button>
+    // </h1>
+// );
 }
